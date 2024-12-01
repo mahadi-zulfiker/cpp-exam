1c6cf68ac6d79b8965ad521b5cfd857e9226fd17
mahade <mahade.adib45@gmail.com>
2024-12-01 21:53:23 +0600

    find jessica problem solved

--- findJessica.cpp
+++ findJessica.cpp
@@ -1,7 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    string S;
+    getline(cin, S);
+
+    string word;
+    stringstream ss(S);
+    bool found = false;
+
+    while (ss >> word)
+    {
+        if (word == "Jessica")
+        {
+            found = true;
+            break;
+        }
+    }
+    if (found)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
     return 0;
 }
